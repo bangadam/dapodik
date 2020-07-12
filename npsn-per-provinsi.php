d1b89b19637e140212497a6b8062dfe12aa4ecb1
bangadam <bangadam.dev@gmail.com>
2020-07-12 10:45:26 +0700

    update

--- npsn-per-provinsi.php
+++ npsn-per-provinsi.php
@@ -26,104 +26,121 @@ $url_prov = $base_url . "index11.php";
 // curl_close($ch_prov);
 // //Get Kabupaten
 # code...
-$noProv = '02';
-$url_kab = $base_url . 'index11.php?kode=' . $noProv . '0000&level=1';
-echo "Proses Kabupaten ... \n";
-$ch_kab = curl_init($url_kab);
-curl_setopt_array(
-	$ch_kab,
-	[
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_SSL_VERIFYPEER => false
-	]
-);
-$get_kab = curl_exec($ch_kab);
-$listKabupaten = $getData->listKabupaten($get_kab);
-//Result Kabupaten
-// echo "==========================================================================================\n";
-// echo "\t\t\t\tDaftar Sekolah " . $nameProv . "\t\t\t\n";
-// echo "||\t\t\t\tData dari Website Data Refrensi\t\t\t\t||\n";
-// echo "||\t\t\t\t\tKemenDikBud\t\t\t\t\t||\n";
-// echo  "==========================================================================================\n";
-$arrSekolah = [];
-$arrSekolah[] = [
-	'npsn' => "NPSN", //tab-1 start
-	'nama' => "Nama Sekolah",
-	'kabupaten_kota' => "Kabupaten/Kota",
-	'provinsi' => "Provinsi",
-	'akreditasi' => "Akreditasi",
-];
-$j = 1;
-foreach ($listKabupaten as $kKab => $vKab) {
-	//Get Kecamatan
-	echo "Proses Kabupaten ..." . $j . " \n";
-	$url_kec = $base_url . $vKab['link'];
-	$ch_kec = curl_init($url_kec);
-	curl_setopt_array($ch_kec, [
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_SSL_VERIFYPEER => false
-	]);
-	$get_kec = curl_exec($ch_kec);
-	$listKecamatan = $getData->listKecamatan($get_kec);
-	//Result Kecamatan
-	// echo "No. " . $j . " -> " . $vKab['kab_name'] . "\n";
-	// echo "\tJml Kecamatan => " . count($listKecamatan) . "\n";
-	$k = 1;
-	foreach ($listKecamatan as $kKec => $vKec) {
-		//Get List NPSN
-		echo "Proses Kecamatan " . $k . "...\n";
-		$url_npsn = $base_url . $vKec['link'];
-		$ch_npsn = curl_init($url_npsn);
-		curl_setopt_array($ch_npsn, [
+$noProvData = ['01', '02', '03', '04', '05'];
+foreach ($noProvData as $noProv) {
+	$url_kab = $base_url . 'index11.php?kode=' . $noProv . '0000&level=1';
+	echo "Proses Kabupaten ... \n";
+	$ch_kab = curl_init($url_kab);
+	curl_setopt_array(
+		$ch_kab,
+		[
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false
+		]
+	);
+	$get_kab = curl_exec($ch_kab);
+	$listKabupaten = $getData->listKabupaten($get_kab);
+	//Result Kabupaten
+	// echo "==========================================================================================\n";
+	// echo "\t\t\t\tDaftar Sekolah " . $nameProv . "\t\t\t\n";
+	// echo "||\t\t\t\tData dari Website Data Refrensi\t\t\t\t||\n";
+	// echo "||\t\t\t\t\tKemenDikBud\t\t\t\t\t||\n";
+	// echo  "==========================================================================================\n";
+	$arrSekolah = [];
+	$arrSekolah[] = [
+		'npsn' => "NPSN", //tab-1 start
+		'nama' => "Nama Sekolah",
+		'alamat' => "Alamat",
+		'kode_pos' => "Kode Pos",
+		'desa_kelurahan' => "Desa/Kelurahan",
+		'kecamatan' => "Kecamatan",
+		'kabupaten_kota' => "Kabupaten/Kota",
+		'provinsi' => "Provinsi",
+		'status' => "Status",
+		'waktu' => "Waktu Penyelenggaraan",
+		'jenjang' => "Jenjang",
+		'naungan' => "Naungan", //tab-2 start
+		'no_sk_pendirian' => "No SK Pendirian",
+		'tgl_sk_pendirian' => "Tanggal SK Pendirian",
+		'no_sk_operasional' => "No SK Operasional",
+		'tgl_sk_operasional' => "Tanggal SK Operasional",
+		'akreditasi' => "Akreditasi",
+		'no_sk_akreditasi' => "No SK Akreditasi",
+		'tgl_sk_akreditasi' => "Tanggal SK Akreditasi",
+		'no_sertifikat_iso' => "No Sertifikat ISO"
+	];
+	$j = 1;
+	foreach ($listKabupaten as $kKab => $vKab) {
+		//Get Kecamatan
+		echo "Proses Kabupaten ..." . $j . " \n";
+		$url_kec = $base_url . $vKab['link'];
+		$ch_kec = curl_init($url_kec);
+		curl_setopt_array($ch_kec, [
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_SSL_VERIFYPEER => false
 		]);
-		$get_npsn = curl_exec($ch_npsn);
-		$listNpsn = $getData->listNpsn($get_npsn);
-		//Result List NPSN
-		// echo "\tNo. " . $k . " -> " . $vKec['kec_name'] . "\n";
-		// echo "\t\tJml Sekolah => " . count($listNpsn) . "\n";
-		$l = 1;
-		foreach ($listNpsn as $kNpsn => $vNpsn) {
-			echo "Listing NPSN ... {$l} \n";
-			$url_sekolah = $base_url . "tabs.php?npsn=" . trim($vNpsn['npsn']);
-			$ch_sekolah = curl_init($url_sekolah);
-			curl_setopt_array($ch_sekolah, [
+		$get_kec = curl_exec($ch_kec);
+		$listKecamatan = $getData->listKecamatan($get_kec);
+		//Result Kecamatan
+		// echo "No. " . $j . " -> " . $vKab['kab_name'] . "\n";
+		// echo "\tJml Kecamatan => " . count($listKecamatan) . "\n";
+		$k = 1;
+		foreach ($listKecamatan as $kKec => $vKec) {
+			//Get List NPSN
+			echo "Proses Kecamatan " . $k . "...\n";
+			$url_npsn = $base_url . $vKec['link'];
+			$ch_npsn = curl_init($url_npsn);
+			curl_setopt_array($ch_npsn, [
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_SSL_VERIFYPEER => false
 			]);
-			$get_sekolah = curl_exec($ch_sekolah);
-			$res = $getData->checkNPSN($get_sekolah);
-			// var_dump($res);
-			// var_dump($get_sekolah);
-			// die();
-			//Result NPSN Sekolah
-			// echo "\t\tNo. " . $l . " -> NPSN " . $vNpsn['npsn'] . "\t" . $vNpsn['nama_sekolah'] . "\n";
-			$l++;
-			$arrSekolah[] = $res;
-			curl_close($ch_sekolah);
+			$get_npsn = curl_exec($ch_npsn);
+			$listNpsn = $getData->listNpsn($get_npsn);
+			//Result List NPSN
+			// echo "\tNo. " . $k . " -> " . $vKec['kec_name'] . "\n";
+			// echo "\t\tJml Sekolah => " . count($listNpsn) . "\n";
+			$l = 1;
+			foreach ($listNpsn as $kNpsn => $vNpsn) {
+				echo "Listing NPSN ... {$l} \n";
+				$url_sekolah = $base_url . "tabs.php?npsn=" . trim($vNpsn['npsn']);
+				$ch_sekolah = curl_init($url_sekolah);
+				curl_setopt_array($ch_sekolah, [
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_SSL_VERIFYPEER => false
+				]);
+				$get_sekolah = curl_exec($ch_sekolah);
+				$res = $getData->checkNPSN($get_sekolah);
+				// var_dump($res);
+				// var_dump($get_sekolah);
+				// die();
+				//Result NPSN Sekolah
+				// echo "\t\tNo. " . $l . " -> NPSN " . $vNpsn['npsn'] . "\t" . $vNpsn['nama_sekolah'] . "\n";
+				$l++;
+				$arrSekolah[] = $res;
+				curl_close($ch_sekolah);
+			}
+			curl_close($ch_npsn);
+			$k++;
 		}
-		curl_close($ch_npsn);
-		$k++;
+		curl_close($ch_kec);
+		$j++;
 	}
-	curl_close($ch_kec);
-	$j++;
-}
-// echo "Total Sekolah di " . $nameProv . " : " . count($arrSekolah) . "\n";
-//Write xlsx
-// ini_set('display_errors', 1);
-// ini_set('log_errors', 1);
-// error_reporting(E_ALL & ~E_NOTICE);
-$filename = "prov_" . $noProv . ".xlsx";
-$writer = new XLSXWriter();
-$writer->setAuthor('bangadam');
-foreach ($arrSekolah as $row) {
-	if ($row != NULL) $writer->writeSheetRow('Sheet1', $row);
-}
-$writer->writeToFile($filename);
-$mv = rename($filename, __DIR__ . "/FILES/" . $filename);
-if ($mv) {
-	echo "File created!\n";
-	echo "DONE!\n";
+	// echo "Total Sekolah di " . $nameProv . " : " . count($arrSekolah) . "\n";
+	//Write xlsx
+	// ini_set('display_errors', 1);
+	// ini_set('log_errors', 1);
+	// error_reporting(E_ALL & ~E_NOTICE);
+	$filename = "prov_" . $noProv . ".xlsx";
+	$writer = new XLSXWriter();
+	$writer->setAuthor('bangadam');
+	foreach ($arrSekolah as $row) {
+		if ($row != NULL) $writer->writeSheetRow('Sheet1', $row);
+	}
+	$writer->writeToFile($filename);
+	$mv = rename($filename, __DIR__ . "/FILES/" . $filename);
+	if ($mv) {
+		echo "File created!\n";
+		echo "DONE!\n";
+	}
 }
 unset($getData);
--- src/func.php
+++ src/func.php
@@ -41,20 +41,20 @@ class GetData
             $npsn = explode('>', $getNpsn)[1];
 
             // //Alamat Sekolah
-            // $getAlamat = explode("</td>", $arrTabOne[2])[3];
-            // $alamat = explode('>', $getAlamat)[1];
+            $getAlamat = explode("</td>", $arrTabOne[2])[3];
+            $alamat = explode('>', $getAlamat)[1];
 
             // //Kode POS Sekolah
-            // $getKodePos = explode("</td>", $arrTabOne[3])[3];
-            // $kodePos = explode('>', $getKodePos)[1];
+            $getKodePos = explode("</td>", $arrTabOne[3])[3];
+            $kodePos = explode('>', $getKodePos)[1];
 
             //Desa/Kelurahan Sekolah
-            // $getDesKel = explode("</td>", $arrTabOne[5])[3];
-            // $desKel = explode('>', $getDesKel)[1];
+            $getDesKel = explode("</td>", $arrTabOne[5])[3];
+            $desKel = explode('>', $getDesKel)[1];
 
             //Kecamatan/Kota
-            // $getKecKot = explode("</td>", $arrTabOne[6])[3];
-            // $kecKot = explode('>', $getKecKot)[1];
+            $getKecKot = explode("</td>", $arrTabOne[6])[3];
+            $kecKot = explode('>', $getKecKot)[1];
 
             //Kabupaten/Kota
             $getKabKot = explode("</td>", $arrTabOne[7])[3];
@@ -65,16 +65,16 @@ class GetData
             $provinsi = explode('>', $getProv)[1];
 
             //Status Sekolah
-            // $getStat = explode("</td>", $arrTabOne[9])[3];
-            // $statusSek = explode('>', $getStat)[1];
+            $getStat = explode("</td>", $arrTabOne[9])[3];
+            $statusSek = explode('>', $getStat)[1];
 
             // //Waktu Penyelenggaraan
-            // $getWp = explode("</td>", $arrTabOne[11])[3];
-            // $waktuPen = explode('>', $getWp)[1];
+            $getWp = explode("</td>", $arrTabOne[11])[3];
+            $waktuPen = explode('>', $getWp)[1];
 
             // //Jenjang Pendidikan
-            // $getJp = explode("</td>", $arrTabOne[13])[3];
-            // $jenjangPen = explode('>', $getJp)[1];
+            $getJp = explode("</td>", $arrTabOne[13])[3];
+            $jenjangPen = explode('>', $getJp)[1];
 
             //tab-2
             $tabTwo = $this->getStringBetween($get, '<div id="tabs-2">', '</div>');
@@ -82,63 +82,63 @@ class GetData
             $arrTabTwo = explode("</tr>", $tableTwo);
 
             //Naungan
-            // $getNaungan = explode("</td>", $arrTabTwo[0])[3];
-            // $naungan = explode(">", $getNaungan)[1];
+            $getNaungan = explode("</td>", $arrTabTwo[0])[3];
+            $naungan = explode(">", $getNaungan)[1];
 
             //No_SK_Pendirian
-            // $getNoSKPendirian = explode("</td>", $arrTabTwo[1])[3];
-            // $noSKPendirian = strpos($getNoSKPendirian, "Perlu Update") ? "Perlu Update" : explode(">", $getNoSKPendirian)[1];
+            $getNoSKPendirian = explode("</td>", $arrTabTwo[1])[3];
+            $noSKPendirian = strpos($getNoSKPendirian, "Perlu Update") ? "Perlu Update" : explode(">", $getNoSKPendirian)[1];
 
             //Tgl_SK_Pendirian
-            // $getTglSKPendirian = explode("</td>", $arrTabTwo[2])[3];
-            // $TglSKPendirian = explode(">", $getTglSKPendirian)[1];
+            $getTglSKPendirian = explode("</td>", $arrTabTwo[2])[3];
+            $TglSKPendirian = explode(">", $getTglSKPendirian)[1];
 
             // No_SK_Operasional
-            // $getNoSKOperasional = explode("</td>", $arrTabTwo[4])[3];
-            // $noSKOperasional = strpos($getNoSKOperasional, "Perlu Update") ? "Perlu Update" : explode(">", $getNoSKOperasional)[1];
+            $getNoSKOperasional = explode("</td>", $arrTabTwo[4])[3];
+            $noSKOperasional = strpos($getNoSKOperasional, "Perlu Update") ? "Perlu Update" : explode(">", $getNoSKOperasional)[1];
 
             // Tgl_SK_Operasional
-            // $getTglSKOperasional = explode("</td>", $arrTabTwo[5])[3];
-            // $tglSKOperasional = explode(">", $getTglSKOperasional)[1];
+            $getTglSKOperasional = explode("</td>", $arrTabTwo[5])[3];
+            $tglSKOperasional = explode(">", $getTglSKOperasional)[1];
 
             // Akreditasi
             $getAkreditasi = explode("</td>", $arrTabTwo[8])[3];
             $akreditasi = $this->getStringBetween($getAkreditasi, '<strong>', '</strong>');
 
             // No_SK_Akreditasi
-            // $getNoSKAkreditasi = explode("</td>", $arrTabTwo[10])[3];
-            // $noSKAkreditasi = explode(">", $getNoSKAkreditasi)[1];
+            $getNoSKAkreditasi = explode("</td>", $arrTabTwo[10])[3];
+            $noSKAkreditasi = explode(">", $getNoSKAkreditasi)[1];
 
             // Tgl_SK_Akreditasi
-            // $getTglSKAkreditasi = explode("</td>", $arrTabTwo[11])[3];
-            // $tglSKAkreditasi = explode(">", $getTglSKAkreditasi)[1];
+            $getTglSKAkreditasi = explode("</td>", $arrTabTwo[11])[3];
+            $tglSKAkreditasi = explode(">", $getTglSKAkreditasi)[1];
 
             // No_Sertifikat_ISO
-            // $getNoSerISO = explode("</td>", $arrTabTwo[13])[3];
-            // $noSerISO = explode(">", $getNoSerISO)[1];
+            $getNoSerISO = explode("</td>", $arrTabTwo[13])[3];
+            $noSerISO = explode(">", $getNoSerISO)[1];
 
             //Result
             $res = [
                 'npsn' => trim($npsn), //tab-1 start
                 'nama' => trim($nama),
-                // 'alamat' => trim($alamat),
-                // 'kode_pos' => trim($kodePos),
-                // 'desa_kelurahan' => trim($desKel),
-                // 'kecamatan' => trim($kecKot),
+                'alamat' => trim($alamat),
+                'kode_pos' => trim($kodePos),
+                'desa_kelurahan' => trim($desKel),
+                'kecamatan' => trim($kecKot),
                 'kabupaten_kota' => trim($kabKot),
                 'provinsi' => trim($provinsi),
-                // 'status' => trim($statusSek),
-                // 'waktu' => trim($waktuPen),
-                // 'jenjang' => trim($jenjangPen),
-                // 'naungan' => trim($naungan), //tab-2 start
-                // 'no_sk_pendirian' => trim($noSKPendirian),
-                // 'tgl_sk_pendirian' => trim($TglSKPendirian),
-                // 'no_sk_operasional' => trim($noSKOperasional),
-                // 'tgl_sk_operasional' => trim($tglSKOperasional),
+                'status' => trim($statusSek),
+                'waktu' => trim($waktuPen),
+                'jenjang' => trim($jenjangPen),
+                'naungan' => trim($naungan), //tab-2 start
+                'no_sk_pendirian' => trim($noSKPendirian),
+                'tgl_sk_pendirian' => trim($TglSKPendirian),
+                'no_sk_operasional' => trim($noSKOperasional),
+                'tgl_sk_operasional' => trim($tglSKOperasional),
                 'akreditasi' => trim($akreditasi),
-                // 'no_sk_akreditasi' => trim($noSKAkreditasi),
-                // 'tgl_sk_akreditasi' => trim($tglSKAkreditasi),
-                // 'no_sertifikat_iso' => trim($noSerISO)
+                'no_sk_akreditasi' => trim($noSKAkreditasi),
+                'tgl_sk_akreditasi' => trim($tglSKAkreditasi),
+                'no_sertifikat_iso' => trim($noSerISO)
             ];
 
             if ($res['nama'] == '') {
